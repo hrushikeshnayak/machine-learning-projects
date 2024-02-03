e86564a7e8590f679fbdb105ebdb7c3f6a23b61d
Hrushikesh Nayak <96150298+hrushikeshnayak@users.noreply.github.com>
2024-02-04 02:02:52 +0530

    Update app.py

--- image_description_generator/app.py
+++ image_description_generator/app.py
@@ -6,9 +6,9 @@ import google.generativeai as genai
 load_dotenv()
 
 prompt = '''Please provide a thorough description of the image, including the objects, people, animals, scenery, colors, textures, and any notable details. Pay attention to the composition, lighting, and overall mood conveyed by the image. Describe any actions or interactions happening within the scene, as well as any emotions or atmosphere you perceive. Additionally, discuss any cultural or historical context that may be relevant to understanding the image. Your description should be detailed and vivid, enabling someone who hasn't seen the image to visualize it as accurately as possible.'''
-
+key  = st.secrets['GOOGLE_API_KEY'] 
 def analyze_image(path):
-    genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+    genai.configure(api_key=key)
     model = genai.GenerativeModel('gemini-pro-vision')
     img = PIL.Image.open(path)
     response = model.generate_content([prompt , img], stream=True)
