a4099bd617a81c6a2b70633dff9d6ac285788750
Hrushikesh Nayak <96150298+hrushikeshnayak@users.noreply.github.com>
2024-02-03 21:37:18 +0530

    Delete  Crop-Prediction directory

---  Crop-Prediction/crop/cropApp/admin.py
+++
@@ -1,8 +0,0 @@
-# from django.contrib import admin
-# from .models import Data
-
-# class DataAdmin(admin.ModelAdmin):
-#     list_display = ('seed_varieties' , 'seed_conditions', 'N', 'P', 'K', 'temp', 'humid', 'ph', 'rain', 'predictions', 'date')
-    
-# admin.site.register(Data, DataAdmin)
-
---  Crop-Prediction/crop/cropApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CropappConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "cropApp"
